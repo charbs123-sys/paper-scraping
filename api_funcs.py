68da41baace4226ec9576099afc3b0310065f2d2
charbs123-sys <charbel.badr04@hotmail.com>
2025-01-28 22:16:57 +1100

    appropriately adding all entry meta data into a dictionary dtype, need to parse pdf file, solution may include local downloads to parsing and deletion.

--- api_funcs.py
+++ api_funcs.py
@@ -1,3 +1,4 @@
+import re
 def find_index(data, index):
     while True:
         if 'title' not in data.entries[0].links[index].keys():
@@ -8,3 +9,29 @@ def find_index(data, index):
         else:
             return index
         return -1
+    
+def entry_metadata (data, entry_data):
+    base = data.entries[0]
+
+    doi = base[entry_data[0]]
+    title = base[entry_data[1]].lower()
+    published = base[entry_data[2]]
+    updated = base[entry_data[3]]
+    summary = base[entry_data[4]].lower()
+    summary = re.sub("\n"," ", summary)
+    
+    authors = base[entry_data[5]] #to normalize later
+    authors = [nam.name.lower() for nam in authors]
+
+    affiliation = base[entry_data[6]].lower()
+
+    mong = {
+        "doi" : doi,
+        "title" : title,
+        "published" : published,
+        "updated" : updated,
+        "summary" : summary,
+        "authors" : authors,
+        "affiliation" : affiliation
+    }
+    return mong
